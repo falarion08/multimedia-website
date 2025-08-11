5fe901b62939a98907c99bcfaeea78158792b479
falarion08 <barrion.ryanonil@gmail.com>
2025-08-12 07:50:03 +0800

    resolved padding issue

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -8,7 +8,10 @@ function App() {
 
   return (
     <>
-  <h1>History of Education in the Philippines</h1>
+    <div className =" absolute top-0 left-0 border-black w-full h-full">
+        <h1 className ="text-left">History of Education in the Philippines</h1>
+
+    </div>
 
     </>
   )
