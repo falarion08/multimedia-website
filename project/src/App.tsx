5dbd8ddd80d7e4ecd521f5ce58cce273a480f706
Ryan <113824865+falarion08@users.noreply.github.com>
2025-08-12 10:18:59 +0000

    updated project folder

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,50 +1,10 @@
-import { Button } from "@/components/ui/button"
-
+import HomepageSection from "./components/sections/homepage"
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div>
+      <HomepageSection/>
     </div>
   )
 }
-
-import * as React from "react"
-
-import { Card, CardContent } from "@/components/ui/card"
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
-
- function CarouselSize() {
-  return (
-    <Carousel
-      opts={{
-        align: "start",
-      }}
-      className="w-full max-w-sm"
-    >
-      <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-3xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
-  )
-}
-
 
 export default App
---
+++ project/src/components/sections/homepage.tsx
@@ -0,0 +1,10 @@
+import { CarouselPlugin } from "../myComponents/carousel"
+export default function HomepageSection(){
+  return(
+    <>
+      <div className="border border-black flex justify-center">
+              <CarouselPlugin/>
+      </div>
+    </>
+  )
+}
